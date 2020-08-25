9d1238fd77df7526f6770b0c62c78ea1d25dde54
vapranav <pranavva34@gmail.com>
2020-08-25 17:56:29 +0530

    Updates

---
+++ precache-manifest.2be1722e560ef1b600f88a935aa4696f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "572e4e2eb62acd34fcb90bf9e69607b0",
+    "url": "/helloworld/index.html"
+  },
+  {
+    "revision": "a1b024486f2bb7755e00",
+    "url": "/helloworld/static/css/main.84af885b.chunk.css"
+  },
+  {
+    "revision": "cebe90d170efc76772af",
+    "url": "/helloworld/static/js/2.b644ef17.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/helloworld/static/js/2.b644ef17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1b024486f2bb7755e00",
+    "url": "/helloworld/static/js/main.c547f704.chunk.js"
+  },
+  {
+    "revision": "62c8ab5aa06a0fe77eed",
+    "url": "/helloworld/static/js/runtime-main.e65e5e73.js"
+  }
+]);
